b3bb9cb06b082e8c5f6ba2ac8729a777b07e6f87
xr-cc <xr-cc@users.noreply.github.com>
2017-11-29 18:52:02 -0600

    list to dict

--- code/parse_data.py
+++ code/parse_data.py
@@ -1,6 +1,11 @@
 import csv
 
+
+data = list()
 with open('../data/pokemon.csv', 'r') as csvfile:
-	dr = csv.reader(csvfile)
-	for row in dr:
-		print(row)
+    dr = csv.reader(csvfile)
+    for row in dr:
+        print(row)
+        data.append(row)
+
+dt = {row[0]: row[1:] for row in data}
