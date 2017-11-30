bf9ff18efbf6dc745d3e5450de36bef3a25a6c61
xr-cc <xr-cc@users.noreply.github.com>
2017-11-29 19:29:06 -0600

    reorder the code

--- code/parse_data.py
+++ code/parse_data.py
@@ -8,13 +8,13 @@ with open('../data/pokemon.csv', 'r') as csvfile:
         data.append(row)
 
 print (data[0])
-data = data[1:]
-dt = {row[0]: row[1:] for row in data}
+data = data[1:] # remove header
 data = np.array(data)
 
-# Types
+# Convert Types to Classes
 type1 = list(np.unique(data[:,2]))
 type2 = list(np.unique(data[:,3]))
+data[:,2] = [type1.index(a)+1 for a in data[:,2]]
+data[:,3] = [type2.index(a)+1 for a in data[:,3]]
 
-type1class = [type1.index(a)+1 for a in data[:,2]]
-type2class = [type2.index(a)+1 for a in data[:,3]]
+dt = {row[0]: list(row[2:]) for row in data[:]}
