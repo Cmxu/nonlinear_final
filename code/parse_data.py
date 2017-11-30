6c8ee78688cfd26c466da3b1afaa40b40554fe52
xr-cc <xr-cc@users.noreply.github.com>
2017-11-29 19:01:27 -0600

    convert types

--- code/parse_data.py
+++ code/parse_data.py
@@ -1,11 +1,20 @@
 import csv
-
+import numpy as np
 
 data = list()
 with open('../data/pokemon.csv', 'r') as csvfile:
     dr = csv.reader(csvfile)
     for row in dr:
-        print(row)
         data.append(row)
 
+print (data[0])
+data = data[1:]
 dt = {row[0]: row[1:] for row in data}
+data = np.array(data)
+
+# Types
+type1 = list(np.unique(data[:,2]))
+type2 = list(np.unique(data[:,3]))
+
+type1class = [type1.index(a)+1 for a in data[:,2]]
+type2class = [type2.index(a)+1 for a in data[:,3]]
