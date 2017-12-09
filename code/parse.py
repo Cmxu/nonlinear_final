7f9a4d9751d6327965787ad76cd526ef9df375f5
Unknown <c.xu@wustl.edu>
2017-12-09 11:45:36 -0600

    2017

--- code/parse.py
+++ code/parse.py
@@ -3,17 +3,25 @@ import numpy as np
 from datetime import datetime
 
 
+only2017 = True
+
 data = list()
 with open('../data/bitstampUSD_1-min_data_2012-01-01_to_2017-10-20.csv', 'r') as csvfile:
     dr = csv.reader(csvfile)
     i = 0
     for row in dr:
         if(row[0] != 'Timestamp'):
-            #time =  datetime.fromtimestamp(int(row[0]))
-            row[0] = i
-            i += 1
-            data.append([float(e) for e in row])
-
+            if(only2017):
+                time =  datetime.fromtimestamp(int(row[0]))
+                if(time.year == 2017):
+                    row[0] = i
+                    i += 1
+                    data.append([float(e) for e in row])
+            else:
+                row[0] = i
+                i += 1
+                data.append([float(e) for e in row])
+                
 print ('Bitcoin File:',data[0])
 #data = data[1:] # remove header
 data = np.array(data)
