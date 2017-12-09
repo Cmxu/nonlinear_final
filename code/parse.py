3b02634297c5596fa06ff470da4dff78d1296058
Unknown <c.xu@wustl.edu>
2017-12-09 11:41:39 -0600

    float

--- code/parse.py
+++ code/parse.py
@@ -6,11 +6,13 @@ from datetime import datetime
 data = list()
 with open('../data/bitstampUSD_1-min_data_2012-01-01_to_2017-10-20.csv', 'r') as csvfile:
     dr = csv.reader(csvfile)
+    i = 0
     for row in dr:
         if(row[0] != 'Timestamp'):
-            time =  datetime.fromtimestamp(int(row[0]))
-            row[0] = time
-            data.append(row)
+            #time =  datetime.fromtimestamp(int(row[0]))
+            row[0] = i
+            i += 1
+            data.append([float(e) for e in row])
 
 print ('Bitcoin File:',data[0])
 #data = data[1:] # remove header
