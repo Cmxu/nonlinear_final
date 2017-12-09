65b3f03dcfc8e14810e051e4ba1b498c9bfb9856
Unknown <c.xu@wustl.edu>
2017-12-09 11:38:24 -0600

    quick fix

--- code/parse.py
+++ code/parse.py
@@ -4,15 +4,15 @@ from datetime import datetime
 
 
 data = list()
-with open('bitcoin-historical-data/bitstampUSD_1-min_data_2012-01-01_to_2017-10-20.csv', 'r') as csvfile:
+with open('../data/bitstampUSD_1-min_data_2012-01-01_to_2017-10-20.csv', 'r') as csvfile:
     dr = csv.reader(csvfile)
     for row in dr:
-        print(row[0])
-        #time =  datetime.fromtimestamp(int(row[0]))
-        #print (time)
-        #data.append(row)
+        if(row[0] != 'Timestamp'):
+            time =  datetime.fromtimestamp(int(row[0]))
+            row[0] = time
+            data.append(row)
 
 print ('Bitcoin File:',data[0])
-data = data[1:] # remove header
+#data = data[1:] # remove header
 data = np.array(data)
 
