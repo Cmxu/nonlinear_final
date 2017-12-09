32cba26843bf4c6c7c7e5c707847e07b56a6e146
Unknown <c.xu@wustl.edu>
2017-12-09 11:49:35 -0600

    function

--- code/parse.py
+++ code/parse.py
@@ -3,26 +3,26 @@ import numpy as np
 from datetime import datetime
 
 
-only2017 = True
+def parseData(currentYear = True):
 
-data = list()
-with open('../data/bitstampUSD_1-min_data_2012-01-01_to_2017-10-20.csv', 'r') as csvfile:
-    dr = csv.reader(csvfile)
-    i = 0
-    for row in dr:
-        if(row[0] != 'Timestamp'):
-            if(only2017):
-                time =  datetime.fromtimestamp(int(row[0]))
-                if(time.year == 2017):
+    data = list()
+    with open('../data/bitstampUSD_1-min_data_2012-01-01_to_2017-10-20.csv', 'r') as csvfile:
+        dr = csv.reader(csvfile)
+        i = 0
+        for row in dr:
+            if(row[0] != 'Timestamp'):
+                if(currentYear):
+                    time =  datetime.fromtimestamp(int(row[0]))
+                    if(time.year == 2017):
+                        row[0] = i
+                        i += 1
+                        data.append([float(e) for e in row])
+                else:
                     row[0] = i
                     i += 1
                     data.append([float(e) for e in row])
-            else:
-                row[0] = i
-                i += 1
-                data.append([float(e) for e in row])
-                
-print ('Bitcoin File:',data[0])
-#data = data[1:] # remove header
-data = np.array(data)
+                    
+    #print ('Bitcoin File:',data[0])
+    #data = data[1:] # remove header
+    return np.array(data)
 
